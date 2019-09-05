bc38e13e192561c01257bc63d01aec9cf12a4e42
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-04 22:46:13 -0500

    add database connection

---
+++ driver/conn.go
@@ -0,0 +1,28 @@
+package driver
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "ruben"
+	password = "agosto14"
+	dbname   = "users"
+)
+
+func Coonection() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Println("error database connection", err)
+	}
+	//defer db.Close()
+	return db
+}
