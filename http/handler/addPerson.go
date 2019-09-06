c24077f4e84400cc10e2ed2547fc24803e8c2d34
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-05 22:39:18 -0500

    add controller

---
+++ http/handler/addPerson.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func CreatePerson(c *gin.Context) {
+	//code
+}
---
+++ http/handler/getPerson.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func GetPerson(c *gin.Context) {
+	//code
+}
