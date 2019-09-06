cc02b211d5164d6751916e8eb9fc063dbc4c2b51
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-05 22:38:01 -0500

    add insert person method test

--- bd/manageUser_test.go
+++ bd/manageUser_test.go
@@ -1,12 +1,29 @@
 package bd
 
 import (
+	"hairdressing/models"
 	"testing"
 )
 
-func TestAddUser(t *testing.T) {
-	value := AddUser()
+func TestGetUser(t *testing.T) {
+	value := GetUser()
 	if value == nil{
 		t.Error("error in get rows")
+	}
+}
+
+func TestAddUser(t *testing.T) {
+
+	p := models.Person {
+		Id:123,
+		Age:24,
+		First_name:"ruben",
+		Last_name:"baez",
+		Email:"rubenba@gmail.com",
+	}
+
+	value := AddUser(&p)
+	if value == nil {
+		t.Error("Error in insert values")
 	}
 }
