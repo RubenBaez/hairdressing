263edc770d864af5142887ceea217af22b9fd80c
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-05 22:40:27 -0500

    add gin controller

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"hairdressing/http/handler"
+)
 
 func main() {
-	fmt.Println("ruben")
+
+	r := gin.Default()
+
+	r.GET("get/client/v1", func(context *gin.Context) {
+		handler.GetPerson(context)
+	})
+
+	r.POST("/create/client/v1", func(context *gin.Context) {
+		handler.CreatePerson(context)
+	})
+
 }
