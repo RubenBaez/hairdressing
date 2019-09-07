697a5dcfe4bdeb98442e74e27ca574b148f0c251
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-07 00:19:16 -0500

    add method of get person

--- http/handler/getPerson.go
+++ http/handler/getPerson.go
@@ -1,7 +1,29 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"hairdressing/bd"
+	"hairdressing/models"
+	"log"
+)
 
 func GetPerson(c *gin.Context) {
-	//code
+	rows := bd.GetUser()
+	for rows.Next() {
+		var person models.Person
+		err := rows.Scan(&person.Id, &person.Age, &person.First_name, &person.Last_name, &person.Email)
+		if err != nil {
+			log.Println("Error in get table rows")
+		}
+		fmt.Println(person.Id, person.Age, person.First_name, person.Last_name, person.Email)
+		value, err := json.Marshal(person)
+		if err != nil {
+			log.Println("Error in person to try parse json")
+		}
+		c.Writer.Header().Set("Content-Type","application/json")
+		c.Writer.WriteHeader(200)
+		c.Writer.Write(value)
+	}
 }
