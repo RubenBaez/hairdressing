f30895eed21c2c545c0d34999f6dca999d4bdfd8
Ruben Baez <rubenbaez1995@gmail.com>
2019-09-16 08:14:20 -0500

    refactor endpoint names

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("get/client/v1", func(context *gin.Context) {
+	router.GET("/api/get/client/v1", func(context *gin.Context) {
 		handler.GetPerson(context)
 	})
 
-	router.POST("/create/client/v1", func(context *gin.Context) {
+	router.POST("/api/create/client/v1", func(context *gin.Context) {
 		handler.CreatePerson(context)
 	})
 
